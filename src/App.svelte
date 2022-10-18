62b5d584c6ee45b83b1a93ad69e4bebd6c762fde
Adrian <adrianthelawx@gmail.com>
2022-10-18 12:06:53 -0600

    Insercion de carpetas y archivos

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Router, Route } from 'svelte-routing';
+    import Login from './views/Login.svelte';
+    import Home from './views/Home.svelte';
+    import NotFound from './views/NotFound.svelte';
+</script>
+
+<Router>
+    <div>
+        <!-- Vistas direccionadas con componentes -->
+        <Route path="/" component={Login} />
+        <Route path="/home" component={Home} />
+        
+        <!-- 404 -->
+        <Route path="*" component={NotFound} />
+    </div>
+</Router>
---
+++ src/views/Home.svelte
@@ -0,0 +1,2 @@
+
+<h1>Soy el home unu</h1>
